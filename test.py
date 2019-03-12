c217bc6ee3d543baef673e5cac72327fe0d35ce0
jiye-ML <woxinxie1234@163.com>
2019-03-12 14:35:22 +0800

    项目提交

---
+++ test.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+from sklearn.model_selection import train_test_split
+
+from sklearn.metrics import r2_score
+
+
+from  sklearn.model_selection import KFold, GridSearchCV
+from  sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import make_scorer
+
+KFold()
+DecisionTreeRegressor()
+make_scorer()
+GridSearchCV()
+
+r2_score()
